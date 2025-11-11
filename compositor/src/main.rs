8192010001acf71dd0b004afb2c1aae7eea8e8c3
Rob Gage <rmg@oakleycomputing.org>
2025-11-10 22:00:50 -0500

    Remove unused command line interface from compositor

--- compositor/src/main.rs
+++ compositor/src/main.rs
@@ -1,14 +1,11 @@
 // Copyright Rob Gage 2025
 
-mod interface;
 mod interpreter;
 
-use clap::Parser;
-use interface::Interface;
 use interpreter::Interpreter;
 
 
 fn main() {
-    let interface: Interface = Interface::parse();
-    interface.handle()
+    let mut interpreter: Interpreter = Interpreter::new();
+    interpreter.run();
 }
